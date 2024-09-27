dc689498223f52ec7c2ebaca207dbfdac1f80a70
vojdminorman <mitshneville7@gmail.com>
2024-09-27 12:59:32 +0100

    Connexion avec le serveur en ligne

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://api-ecommerce-express-e5kr.onrender.com//',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
